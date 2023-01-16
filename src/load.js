98f979fe4ecad320c52ace6b5067207cea4edd7b
Jake Dahlgren <82679852+jake-dahlgren@users.noreply.github.com>
2023-01-16 16:44:49 -0600

    fix image display errors

--- src/load.js
+++ src/load.js
@@ -270,7 +270,7 @@ body {
 
 .item-img-source {
 	align-items: center;
-	background: #f1592b;
+	background: #b29bc2;
 	border-radius: 50%;
 	box-shadow: 0 1px 1px 0 rgb(0 0 0 / 22%), 0 2px 2px 0 rgb(0 0 0 / 12%);
 	display: flex;
